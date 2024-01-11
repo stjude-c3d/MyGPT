5ca0e984971b2f4e675467e07afe7482e9779dae
caprizone6 <caprizone6@gmail.com>
2024-01-10 18:12:47 -0600

    Add GitHub Actions workflows for backend and frontend

--- installation/pc_with_gpu/macOS/start_nginx.sh
+++ installation/pc_with_gpu/macOS/start_nginx.sh
@@ -1,4 +1,6 @@
 cd MyGPT
 brew install nginx
 scp llm_api/nginx.conf /opt/homebrew/etc/nginx/nginx.conf
+# TODO: might remove above line
+scp llm_api/nginx.conf /usr/local/etc/nginx/nginx.conf
 brew services reload nginx
