77cbe8d0c42a8336d8a41bd2ea057a4ae7100fae
Jude Downing <judedowning0805@gmail.com>
2024-06-20 11:03:32 -0500

    evaluation clean-up

--- evaluation/utils/collect_eval_answers.py
+++ evaluation/utils/collect_eval_answers.py
@@ -8,16 +8,13 @@ import json
 answer_api = 'https://svlpgpt001a.stjude.org/api/generate/'
 
 # Load evaluation documents and questions
-eval_doc = pd.read_csv('evaluation/documents/mygpt_eval_doc.csv', encoding = 'ISO-8859-1').dropna()
+eval_doc = pd.read_csv('evaluation/documents/eval_dataset.csv', encoding = 'ISO-8859-1').dropna()
 question_list = eval_doc['question'].tolist()
 groundtruth_list = eval_doc['ground_truth'].tolist()
-lib_list = eval_doc['library'].tolist()
 
 # List of models and embeddings to evaluate
 models = ['llama3:latest', 'gemma:latest', 'mistral:latest', 'llama3:70b', 'llama2:latest', 'vicuna:latest']
-old_embeds = ['all-MiniLM-L6-v2', 'all-MiniLM-L12-v2', 'multi-qa-MiniLM-l6-cos-v1', 'all-mpnet-base-v2', 'multi-qa-mpnet-base-dot-v1', 'paraphrase-albert-small-v2']
-embeds = ['multi-qa-MiniLM-l6-cos-v1', 'all-MiniLM-L6-v2', 'all-MiniLM-L12-v2', 'multi-qa-mpnet-base-dot-v1']
-embed_shorthands = ['qa-cos', 'mini-l6', 'mini-l12', 'qa-dot']
+embed_shorthands = ['qa-cos', 'mini-l12', 'snowflake']
 datasets = ['mygpt-GPCR', 'mygpt-Kinase']
 
 # Function to query APIs with payload and measure time taken
@@ -35,50 +32,49 @@ def query_api(url, payload):
         return None
 
 # Main processing loop
-for embed, shorthand in zip(embeds, embed_shorthands):
+for shorthand in embed_shorthands:
+    dataset_name = f'mygpt-all-{shorthand}'
+    with open(f'evaluation/utils/eval_context/{shorthand}/{dataset_name}.json') as f:
+        context_lists = pd.DataFrame.from_dict(json.loads(f.read()))['contexts'].tolist()
     # Process each model for QA
     for model in models:
-        for j, (question, library, groundtruth) in enumerate(zip(question_list, lib_list, groundtruth_list)):
-            if f'mygpt-{library}' in datasets:
-                # Log query info
-                print('\n')
-                print(f'MODEL: {model}; EMBEDDING: {embed};')
-                print(f'Loading Question {str(j+1)}...')
+        for j, (question, groundtruth, contexts) in enumerate(zip(question_list, groundtruth_list, context_lists)):
+            # Log query info
+            print('\n')
+            print(f'MODEL: {model}; EMBEDDING: {shorthand};')
+            print(f'Loading Question {str(j+1)}...')
 
-                dataset_name = f'mygpt-{library}-{shorthand}'
-                contexts = pd.DataFrame.from_records(f'evaluation/eval_context/{shorthand}/{dataset_name}.json')['context'].tolist()[j]
-
-                # Generate answer prompt (assuming query_api function remains unchanged)
-                system_prompt = 'Use following information to answer the question in less than 100 words, try not to use anything else: ' + str(contexts)
-                answer_prompt = {
-                    "model": model,
-                    "prompt": question,
-                    "stream": False,
-                    'system': system_prompt,
-                    "options": {
-                        "temperature": 0.4,
-                        "top_k": 20,
-                        "top_p": 0.7
-                    }
+            # Generate answer prompt (assuming query_api function remains unchanged)
+            system_prompt = 'Use following information to answer the question in less than 100 words, try not to use anything else: ' + str(contexts)
+            answer_prompt = {
+                "model": model,
+                "prompt": question,
+                "stream": False,
+                'system': system_prompt,
+                "options": {
+                    "temperature": 0.4,
+                    "top_k": 20,
+                    "top_p": 0.7
                 }
+            }
 
-                # Choose API endpoint based on model type (assuming query_api function remains unchanged)
-                answer = query_api(answer_api, answer_prompt).get('response', '')
+            # Choose API endpoint based on model type (assuming query_api function remains unchanged)
+            answer = query_api(answer_api, answer_prompt).get('response', '')
 
-                # Store QA results
-                if answer:
-                    qa_result = {'question': question, 'context': contexts, 'answer': answer, 'ground_truth': groundtruth}
-                    # Prepare file path
-                    model_name = model.removesuffix(":latest").replace(":", "-")
-                    result_file_path = f'evaluation/utils/eval_answers/{model_name}/results-{model_name}-{shorthand}.json'
+            # Store QA results
+            if answer:
+                qa_result = {'question': question, 'context': contexts, 'answer': answer, 'ground_truth': groundtruth}
+                # Prepare file path
+                model_name = model.removesuffix(":latest").replace(":", "-")
+                result_file_path = f'evaluation/utils/eval_answers/{model_name}/results-{model_name}-{shorthand}.json'
 
-                    # Check if the file exists to decide whether to append or create new
-                    if os.path.exists(result_file_path):
-                        with open(result_file_path, 'r+') as result_file:
-                            result_data = json.load(result_file)
-                            result_data.append(qa_result)
-                            result_file.seek(0)
-                            json.dump(result_data, result_file, indent=4)
-                    else:
-                        with open(result_file_path, 'w') as result_file:
-                            json.dump([qa_result], result_file, indent=4)
+                # Check if the file exists to decide whether to append or create new
+                if os.path.exists(result_file_path):
+                    with open(result_file_path, 'r+') as result_file:
+                        result_data = json.load(result_file)
+                        result_data.append(qa_result)
+                        result_file.seek(0)
+                        json.dump(result_data, result_file, indent=4)
+                else:
+                    with open(result_file_path, 'w') as result_file:
+                        json.dump([qa_result], result_file, indent=4)
