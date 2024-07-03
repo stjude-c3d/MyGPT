bcc876f61e60326fa7eed32ac88fa69996e2ad83
Jude Downing <judedowning0805@gmail.com>
2024-07-03 10:23:06 -0500

    Answer Correctness Scores By Chunksize

--- evaluation/utils/answer_datasheet_constructor.py
+++ evaluation/utils/answer_datasheet_constructor.py
@@ -31,25 +31,28 @@ def reconstruct_answer_data(feature, shorthands, models, folder, suffix, num_q):
     final_df = pd.DataFrame.from_records(data=data)
     final_df.to_csv(f'evaluation/documents/{feature}.csv')
 
-def reconstruct_chunksize_data(feature):
+def reconstruct_chunksize_data(feature, prefix, folder):
     dataset = {
         'with_overlap': [],
-        'chunksize_500': [],
-        'chunksize_1000': [],
-        'chunksize_1500': []
+        '500 Characters': [],
+        '1000 Characters': [],
+        '1500 Characters': []
     }
     for overlap in [True, False]:
         add_overlap = True
         for chunksize in ['500', '1000', '1500']:
             if overlap:
-                input_path = f'evaluation/scores/param_context_scores/recall-{chunksize}-overlap.csv'
+                input_path = f'evaluation/scores/parameter_scores/{folder}/{prefix}-{chunksize}-overlap.csv'
             else:
-                input_path = f'evaluation/scores/param_context_scores/recall-{chunksize}.csv'
+                input_path = f'evaluation/scores/parameter_scores/{folder}/{prefix}-{chunksize}.csv'
             df = pd.read_csv(input_path)
             for x in df[feature].tolist():
-                dataset[f'chunksize_{chunksize}'].append(x)
+                dataset[f'{chunksize} Characters'].append(x)
                 if add_overlap:
-                    dataset['with_overlap'].append(overlap)
+                    if overlap:
+                        dataset['with_overlap'].append("Overlap")
+                    else:
+                        dataset['with_overlap'].append("No Overlap")
             add_overlap = False
     final_df = pd.DataFrame.from_dict(dataset)
     final_df.to_csv(f'evaluation/documents/chunksize_{feature}.csv')
@@ -57,4 +60,4 @@ def reconstruct_chunksize_data(feature):
 
 shorthands = ['qa-cos', 'mini-l6', 'snowflake']
 models = ['gemma', 'llama2', 'llama3', 'llama3-70b', 'mistral', 'vicuna']
-reconstruct_answer_data('answer_correctness', shorthands, models, 'correctness_scores', 'correctness', 232)
+reconstruct_chunksize_data('answer_correctness', 'correctness', 'answer_correctness')
