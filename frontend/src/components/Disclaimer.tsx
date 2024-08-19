b2663cfd2c2d717806a6d238752583bb784b0905
caprizone6 <caprizone6@gmail.com>
2024-08-19 15:08:57 -0500

    add azure build script

--- frontend/src/components/Disclaimer.tsx
+++ frontend/src/components/Disclaimer.tsx
@@ -92,7 +92,9 @@ const Disclaimer = (props: DisclaimerProps) =>{
 			<div className={'bg-panel1 w-3/4 max-h-[750px] max-w-[1200px] rounded-lg ' + (window.screen.availHeight < 1000 ? 'h-[75vh]' : 'h-[65vh]')}>
 				<div className='flex justify-between'>
 					<div className='text-2xl font-bold text-white mt-8 mx-8'>Limited Access Statement</div>
-					<div className='text-2xl font-bold text-white mt-8 mr-8 cursor-pointer' onClick={props.closeDisclaimer}>x</div>
+					{djangoAuthenticated && !disclaimerAgreed ?
+					<></>:
+					<div className='text-2xl font-bold text-white mt-8 mr-8 cursor-pointer' onClick={props.closeDisclaimer}>x</div>}
 				</div>
 				<div className='bg-panel2 w-full my-2 py-2 h-[85%]'>
 					<div className='text-nav m-8'>
