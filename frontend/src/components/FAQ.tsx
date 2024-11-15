e72368b8874dd0c61dc84626a2841f98ac1b298f
jonjno <sheeja4106@gmail.com>
2024-11-15 17:03:10 -0600

    added faq for question and answer

--- frontend/src/components/FAQ.tsx
+++ frontend/src/components/FAQ.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import { faq } from "../utils/DefaultState";
+
 interface FAQProps {
 	closeFAQ: any,	
 }
 
 const FAQ = (props:FAQProps) =>{
 
+	const [activeCategory, setActiveCategory] = useState<string | null>(null);
 	return (
 		// create floating panel with opque background
 		<div className='fixed inset-0 z-50 bg-black bg-opacity-70 flex items-center justify-center'>
@@ -12,21 +16,34 @@ const FAQ = (props:FAQProps) =>{
 					<div className='text-2xl font-bold text-white mt-8 mx-8'>FAQs</div>
 					<div className='text-2xl font-bold text-white mt-8 mr-8 cursor-pointer' onClick={props.closeFAQ}>x</div>
 				</div>
-				<div className={'flex justify-between my-6 '+ (window.screen.availHeight < 1000 ? 'h-[78vh]' : 'h-[62vh]')}>
+				<div className={'flex justify-between my-6 '+ (window.screen.availHeight < 1000 ? 'h-[78vh]' : 'h-[60vh]')}>
 					{/* create left side panel for questions */}
 					<div className={'w-1/3 border-slate-400 border-y-2 overflow-y-auto ' + (window.screen.availHeight < 1000 ? 'h-[80vh]' : 'h-[62vh]')}>
 						<div className='grid grid-cols-1 divide-y'>
-							<div className='text-white py-2 px-4'>
-								
-							{/* dropdown for datasets */}
-							
+							{Object.keys(faq).map((category) => (
+                            <div key={category} className='w-full'>
+							<div className={`text-white text-xl cursor-pointer p-2 border-b border-white ${activeCategory === category ? 'font-normal bg-nav' : 'font-light bg-panel1 dark:bg-panel4-dark'}`}
+							  onClick={() => setActiveCategory(category)}>
+							  <h2>{category}</h2>
 							</div>
-							
+						  </div>
+						   ))}	
 						</div>
 					</div>
 					{/* create right side for answers and sources list */}
 					<div className={'w-2/3 bg-panel3 dark:bg-neutral-800 overflow-y-auto overflow-x-clip border-slate-400 border-y-2 ' + (window.screen.availHeight < 1000 ? 'h-[80vh] max-h-[80vh]' : 'h-[62vh] max-h-[62vh]')}>
-					
+					{activeCategory && (
+              <div>
+				<div className='text-panel1 text-lg font-bold'>{activeCategory}</div>
+				
+                {faq[activeCategory]?.map((item, index) => (
+                  <div key={index}>
+					<div className='text-nav dark:text-nav-dark p-2 mt-2 flex justify-start text-lg font-semibold'>{item.question}  </div>
+					<div className='text-nav dark:text-nav-dark p-2'dangerouslySetInnerHTML={{ __html: item.answer }}></div>
+                  </div>
+                ))}
+              </div>
+            )}
 					</div>
 				</div>
 			</div>
